ed61bc6f8ced9c953560a65c7f64b933cb931858
Kirill-mol <kir.mololkin@yandex.ru>
2021-04-23 00:40:25 +0300

    CORS

--- src/main/java/ru/mololkin/investingsandbox/config/SecurityConfig.java
+++ src/main/java/ru/mololkin/investingsandbox/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package ru.mololkin.investingsandbox.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -59,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("*");
     }
 }

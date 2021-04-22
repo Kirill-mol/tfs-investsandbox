d9e74671f2573968770aa91cd0e06bf2785ed124
Kirill-mol <kir.mololkin@yandex.ru>
2021-04-22 23:17:52 +0300

    CORS

--- src/main/java/ru/mololkin/investingsandbox/config/SimpleCORSFilter.java
+++ src/main/java/ru/mololkin/investingsandbox/config/SimpleCORSFilter.java
@@ -17,7 +17,8 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "*");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		response.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
 		chain.doFilter(req, res);
 	}
 
